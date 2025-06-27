2869c9b5f347d2fe0043145cdd9ffdb303d9a3fd
Tim Heuett <tim.heuett@gmail.com>
2025-06-27 14:26:11 -0700

    tests for markdown.follow_external_link

--- tests/markdown_spec.lua
+++ tests/markdown_spec.lua
@@ -128,6 +128,74 @@ describe("markdown.follow_journal_link", function()
   end)
 end)
 
+describe("markdown.follow_external_link", function()
+  local get_current_line_stub
+  local win_get_cursor_stub
+  local vim_ui_open_stub
+
+  before_each(function()
+    get_current_line_stub = stub(vim.api, "nvim_get_current_line")
+    vim_ui_open_stub = stub(vim.ui, "open")
+    win_get_cursor_stub = stub(vim.api, "nvim_win_get_cursor")
+  end)
+
+  after_each(function()
+    get_current_line_stub:revert()
+    vim_ui_open_stub:revert()
+    win_get_cursor_stub:revert()
+  end)
+
+  it("opens a single link on the line and does not fall through", function()
+    get_current_line_stub.returns("[link](http://example.com)")
+    win_get_cursor_stub.returns({ 1, 1 })
+
+    assert(markdown.follow_external_link() == "")
+    vim.wait(0) -- force async execution
+
+    assert.stub(vim_ui_open_stub).was_called_with("http://example.com")
+  end)
+
+  it("opens a single link anywhere on the line regardless of cursor position and does not fall through", function()
+    get_current_line_stub.returns("- hey check out this cool [link](http://example.com)")
+    win_get_cursor_stub.returns({ 1, 1 }) -- cursor is not on the link
+    
+    assert(markdown.follow_external_link() == "")
+    vim.wait(0) -- force async execution
+
+    assert.stub(vim_ui_open_stub).was_called_with("http://example.com")
+  end)
+
+  it("opens a link under the cursor if there are multiple links on the line and does not fall through", function()
+    get_current_line_stub.returns("- here are some neat links: [link1](http://example1.com) and [link2](http://example2.com)")
+    win_get_cursor_stub.returns({ 1, 30 }) -- cursor is on link1
+    
+    assert(markdown.follow_external_link() == "")
+    vim.wait(0) -- force async execution
+
+    assert.stub(vim_ui_open_stub).was_called_with("http://example1.com")
+  end)
+
+  it("passes on to the next handler if there are no links", function()
+    get_current_line_stub.returns("No links here")
+    win_get_cursor_stub.returns({ 1, 1 })
+
+    assert(markdown.follow_external_link() == config.options.markdown.follow_external_link_keybind)
+    vim.wait(0) -- force async execution
+
+    assert.stub(vim_ui_open_stub).was_not_called()
+  end)
+
+  it("passes on to the next handler if there are multiple links and the cursor is not on one of them", function()
+    get_current_line_stub.returns("- here are some neat links: [link1](http://example1.com) and [link2](http://example2.com)")
+    win_get_cursor_stub.returns({ 1, 1 })
+    
+    assert(markdown.follow_external_link() == config.options.markdown.follow_external_link_keybind)
+    vim.wait(0) -- force async execution
+
+    assert.stub(vim_ui_open_stub).was_not_called()
+  end)
+end)
+
 describe("markdown.install", function()
   local keymap_set_stub
 
